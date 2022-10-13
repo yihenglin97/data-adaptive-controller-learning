b4a8f963a11d9ecbbb4e8b4d722216595dfb59a9
James Preiss <jamesalanpreiss@gmail.com>
2022-10-12 19:17:36 -0700

    factor out gradient estimate from MPC

--- MPCLTI.py
+++ MPCLTI.py
@@ -2,6 +2,7 @@ from numba import njit
 import numpy as np
 from scipy import linalg as la
 
+from GAPS import GAPSEstimator
 import Controller
 
 
@@ -15,11 +16,10 @@ def _step(multipliers, multipliersA, max_k, param, control_action, predicted_dis
         control_action += param[i]*grads[:, i] + multipliersA[i]@(V[i] - V[i+1])
     return grads
 
+
 class MPCLTI(Controller.Controller):
     def __init__(self, initial_param, buffer_length, learning_rate, horizon=None):
         super().__init__(initial_param, buffer_length, learning_rate)
-        self.partial_u_theta = []
-        self.partial_u_x = []
         self.param_history = [initial_param]
         self.A = None
         self.B = None
@@ -36,6 +36,7 @@ class MPCLTI(Controller.Controller):
         else:
             assert initial_param.size == 1
             self.max_k = horizon
+        self.estimator = GAPSEstimator(buffer_length)
 
     def decide_action(self, state, context):
         is_terminal, predicted_disturbances, V, sys_params = context
@@ -57,49 +58,28 @@ class MPCLTI(Controller.Controller):
             self.m = self.B.shape[1]
         k = predicted_disturbances.shape[1]
         control_action = - self.K@(state - V[:, 0])
-        self.partial_u_x.append(-self.K)
+        dudx = -self.K
         if self.param.size > 1:
             # Copy to make contiguous.
             grads = _step(self.Multipliers, self.MultipliersA, self.max_k, self.param, control_action, predicted_disturbances.T.copy(), V.T.copy())
             # Mutates control_action
-            self.partial_u_theta.append(grads)
+            dudtheta = grads
         else:
             grad = np.zeros_like(control_action)
             for i in range(k):
                 this_grad = self.Multipliers[i]@(predicted_disturbances[:, i])
                 control_action += (self.param*this_grad + self.Multipliers[i]@(self.A@V[:, i] - V[:, i+1]))
                 grad += this_grad
-            self.partial_u_theta.append(grad)
+            dudtheta = grad
 
+        self.estimator.add_partial_u(dudx, dudtheta)
         return control_action
 
     def update_param(self, grads):
-        partial_f_x, partial_f_u, partial_g_x, partial_g_u = grads
-        new_buffer = []
-        G = None
-        if len(self.buffer) == 0:
-            G = partial_f_u @ self.partial_u_theta[-1]
-            # update the buffer
-            new_buffer = [self.partial_u_theta[-1]]
-        else:
-            grad_sum = partial_g_u @ self.buffer[0]
-            current_buffer_len = len(self.buffer)
-            new_buffer = [self.partial_u_theta[-1], partial_g_u @ self.buffer[0]]
-            premul = partial_g_x + partial_g_u @ self.partial_u_x[-2]
-            if current_buffer_len > 1:
-                Brest = self.buffer[1:]
-                new = premul @ Brest
-                grad_sum += np.sum(new, axis=0)
-                new_buffer.extend(list(new))
-            G = (partial_f_x + partial_f_u @ self.partial_u_x[-1])@grad_sum + partial_f_u @ self.partial_u_theta[-1]
-
+        G = self.estimator.update(*grads)
         new_param = self.param - self.learning_rate * G
         # the projection step
         new_param = np.clip(new_param, 0.0, 1.0)
         self.param = new_param
         self.param_history.append(new_param)
-        if len(new_buffer) > self.buffer_length:
-            self.buffer = new_buffer[:self.buffer_length]
-        else:
-            self.buffer = new_buffer
         return self.param
